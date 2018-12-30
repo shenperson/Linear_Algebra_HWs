ccf13a07c879d961a5d7273cd161f678815a186d
shenperson <wayne19990609@gmail.com>
2018-12-30 22:41:00 +0800

    report unfinished

---
+++ hw6/hw6_b06901090.py
@@ -0,0 +1,109 @@
+from PIL import Image
+import numpy as np
+import matplotlib
+
+
+def load_image(img_path):
+    """Load image into a 3D numpy array
+    Arg:
+        img_path: string, file path of the image file.
+    Return:
+        imArr: numpy array with shape (height, width, 3).
+    """
+    with Image.open(img_path) as im:
+        imArr = np.fromstring(im.tobytes(), dtype=np.uint8)
+        # imArr = imArr.reshape((im.size[1], im.size[0]))
+        imArr = imArr.reshape((im.size[1], im.size[0], 3))
+    return imArr
+
+
+def load_image2(img_path):
+    """Load image into a 3D numpy array
+    Arg:
+        img_path: string, file path of the image file.
+    Return:
+        imArr: numpy array with shape (height, width, 3).
+    """
+    with Image.open(img_path) as im:
+        imArr = np.fromstring(im.tobytes(), dtype=np.uint8)
+        imArr = imArr.reshape((im.size[1], im.size[0]))
+        # imArr = imArr.reshape((im.size[1], im.size[0], 3))
+    return imArr
+
+
+def save_image(imArr, fpath='output.jpg'):
+    """Save numpy array as a jpeg file
+    Arg:
+        imArr: 2d or 3d numpy array, *** it must be np.uint8 and range from [0, 255]. ***
+        fpath: string, the path to save imgArr.
+    """
+    im = Image.fromarray(imArr)
+    im.save(fpath)
+
+
+def svd_compress(imArr, K=50):
+    """Compress image array using SVD decomposition.
+    Arg:
+        imArr: numpy array with shape (height, width, 3).
+    Return:
+        Compressed imArr: numpy array.
+    """
+    imArr_compressed = np.zeros(imArr.shape)
+    # For each channel
+    for ch in range(3):
+        # --------------------
+        # TODO:
+        #     Compress the image array using SVD decomposition
+        # hint:
+        #     1. numpy.linalg.svd
+        #     2. numpy.diag
+        #     3. numpy.dot
+        #
+        # Your code here
+        # imArr_compressed = ??
+        #
+        # --------------------
+        U, sigma, VT = np.linalg.svd(imArr[:, :, ch], full_matrices=False)
+        sigma[K:] = 0.
+        imArr_compressed[:, :, ch] = np.dot(U*sigma, VT)
+        # Make imArr_compressed range from 0 to 255
+        imArr_compressed[:, :, ch] -= imArr_compressed[:, :, ch].min()
+        imArr_compressed[:, :, ch] /= imArr_compressed[:, :, ch].max()
+        imArr_compressed[:, :, ch] *= 255
+        # Return uint8 because save_image needs input of type uint8
+    return imArr_compressed.astype(np.uint8)
+
+
+if __name__ == "__main__":
+    img_path = 'img/vegetable_english.jpg'
+    imArr = load_image(img_path)
+    """
+    for i in range(1, 1000):
+        imArr_compressed = np.zeros(imArr.shape)
+        print(i)
+        ch = 1
+        U, sigma, VT = np.linalg.svd(imArr[:, :, ch], full_matrices=False)
+        temp = np.array(sigma[i])
+        sigma[:] = 0.
+        sigma[i] = temp
+        imArr_compressed[:, :, ch] = np.dot(U*sigma, VT)
+        # Make imArr_compressed range from 0 to 255
+        imArr_compressed[:, :, ch] -= imArr_compressed[:, :, ch].min()
+        imArr_compressed[:, :, ch] /= imArr_compressed[:, :, ch].max()
+        imArr_compressed[:, :, ch] *= 255
+        # imArr_compressed[..., 0] = 0.
+        # imArr_compressed[..., 2] = 0.
+        imArr_compressed = imArr_compressed[:, :, 1]
+        print(imArr_compressed.shape)
+        imArr_compressed = imArr_compressed.astype(np.uint8)
+        print(imArr_compressed.shape)
+        save_image(imArr_compressed, 'test/test'+str(i)+'.jpg')
+    # """
+    img = load_image2('test/test1.jpg')
+    for i in range(0, 500):
+        try:
+            img += load_image2('test/test'+str(i)+'.jpg')
+        except:
+            pass
+    print(max(img[0]))
+    save_image(img, 'img.jpg')
---
+++ hw6/main.py
@@ -0,0 +1,74 @@
+from hw6_b06901090 import svd_compress
+from PIL import Image
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+
+def load_image(img_path):
+    """Load image into a 3D numpy array
+    Arg:
+        img_path: string, file path of the image file.
+    Return:
+        imArr: numpy array with shape (height, width, 3).
+    """
+    with Image.open(img_path) as im:
+        imArr = np.fromstring(im.tobytes(), dtype=np.uint8)
+        imArr = imArr.reshape((im.size[1], im.size[0], 3))
+    return imArr
+
+
+def save_image(imArr, fpath='output.jpg'):
+    """Save numpy array as a jpeg file
+    Arg:
+        imArr: 2d or 3d numpy array, *** it must be np.uint8 and range from [0, 255]. ***
+        fpath: string, the path to save imgArr.
+    """
+    im = Image.fromarray(imArr)
+    im.save(fpath)
+
+
+def plot_curve(k, err, fpath='curve.png', show=False):
+    """Save the relation curve of k and approx. error to fpath
+    Arg:
+        k: a list of k, in this homework, it should be [1, 5, 50, 150, 400, 1050, 1289]
+        err: a list of aprroximation error corresponding to k = 1, 5, 50, 150, 400, 1050, 1289
+        fpath: string, the path to save curve
+        show: boolean, if True: display the plot else save the plot to fpath
+    """
+    plt.gcf().clear()
+    plt.plot(k, err, marker='.')
+    plt.title('SVD compression')
+    plt.xlabel('k')
+    plt.ylabel('Approx. error')
+    if show:
+        plt.show()
+    else:
+        plt.savefig(fpath, dpi=300)
+
+
+def approx_error(imArr, imArr_compressed):
+    """Calculate approximation error 
+    Arg:
+        Two numpy arrays
+    Return:
+        A float number, approximation error
+    """
+    v = imArr.ravel().astype(float)
+    u = imArr_compressed.ravel().astype(float)
+    return np.linalg.norm(v - u) / len(v)
+
+
+img_path = 'img/vegetable_english.jpg'
+imArr = load_image(img_path)
+
+ks = [1, 5, 50, 150, 400, 1050, 1289]
+err = []
+for k in ks:
+    print("Perform SVD for k=%d ..." % k, end='\r')
+    imArr_compressed = svd_compress(imArr, K=k)
+    err += [approx_error(imArr, imArr_compressed)]
+    save_image(imArr_compressed, 'result_{}.jpg'.format(k))
+
+plot_curve(ks, err, show=False)
